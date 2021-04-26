4b6776a394f6d2ebdc90fdff9fd25fe8bf44039e
Timothy Do <tydo83@gmail.com>
2021-04-26 12:54:48 -0400

    added spinner.css and .js in spinner folder

--- src/MainRouter.js
+++ src/MainRouter.js
@@ -1,9 +1,22 @@
 import React from 'react'
 import { Switch, Route } from 'react-router-dom'
 
+const Navbar = React.lazy(() => import('./components/Navbar/Navbar'))
+const Home = React.lazy(() => import('./components/Home/Home'))
+const Login = React.lazy(() => import('./components/Login/Login'))
+const SignUp = React.lazy(() => import('./components/Signup/Signup'))
+
 function MainRouter() {
-    return 
-        <Route Path=""></Route>
+    return (
+        <>
+            <Navbar />
+            <Switch>
+                <Route Path="/" component={Home} />
+                <Route Path="/login" component={Login} />
+                <Route Path="/signup" component={SignUp} />
+            </Switch>
+        </>
+    )
 }
 
 export default MainRouter
