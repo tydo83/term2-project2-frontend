4375039d32bffb8b5b0963fc32aea84842618411
Timothy Do <tydo83@gmail.com>
2021-04-26 14:00:08 -0400

    added navbar using material-ui

--- src/App.js
+++ src/App.js
@@ -1,16 +1,38 @@
-import React from 'react'
+import { createMuiTheme } from '@material-ui/core/styles'
+import { ThemeProvider } from '@material-ui/styles'
+import { deepPurple } from '@material-ui/core/colors'
+
 import { BrowserRouter as Router } from 'react-router-dom'
 
+import React from 'react'
 import MainRouter from './MainRouter'
 import Spinner from './components/Spinner/Spinner'
 
+const theme = createMuiTheme({
+  palette: {
+    primary: {
+      main: deepPurple["A200"],
+      contrastText: "#fff",
+    },
+    secondary: {
+      main: "#FFF5EE",
+      contrastText: "black",
+    },
+  },
+  typography: {
+    fontFamily: ["Roboto", "Helvetica Neue", "Arial", "sans-serif"].join(","),
+  },
+});
+
 function App() {
   return (
+    <ThemeProvider theme={theme}>
     <React.Suspense fallback={<Spinner />}>
       <Router>
         <MainRouter />
       </Router>
-    </React.Suspense>
+    </React.Suspense>        
+    </ThemeProvider>
   );
 }
 
--- src/components/Navbar/Navbar.js
+++ src/components/Navbar/Navbar.js
@@ -1,9 +1,46 @@
 import React from 'react'
+import { makeStyles } from '@material-ui/core/styles'
+import { AppBar, Toolbar, Typography, Button } from '@material-ui/core'
+import { NavLink } from 'react-router-dom'
+
+const useStyle = makeStyles(() => ({
+    root: {
+        flexGrow: 1,
+    },
+    title: {
+        flexGrow: 1,
+    }
+}))
 
 function Navbar() {
+    const classes = useStyle();
+
     return (
-        <div>
-            Navbar
+        <div className={classes.root}>
+            <AppBar position="static">
+                <Toolbar>
+                    <Typography variant="h6" className={classes.title}>
+                        Admin Dashboard
+                    </Typography>
+                    <NavLink
+                        to="/login"
+                        exact 
+                        className="nav-link"
+                        activeClassName="active-nav-link"
+                    >
+                        <Button color="inherit">Login</Button>
+                    </NavLink>
+                    
+                    <NavLink
+                        to="/sign-up"
+                        exact 
+                        className="nav-link"
+                        activeClassName="active-nav-link"
+                    >
+                        <Button color="inherit">Sign Up</Button>
+                    </NavLink>
+                </Toolbar>
+            </AppBar>
         </div>
     )
 }
