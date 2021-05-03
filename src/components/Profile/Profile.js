e3254b453d6eb775b911ff49892c04082bb0546a
Timothy Do <tydo83@gmail.com>
2021-05-03 14:16:15 -0400

    final wet ver

--- src/components/Profile/Profile.js
+++ src/components/Profile/Profile.js
@@ -1,5 +1,9 @@
 import React, { useState, useEffect } from 'react'
 import { makeStyles } from '@material-ui/core/styles'
+import jwtDecode from 'jwt-decode'
+import { toast } from 'react-toastify'
+import axios from 'axios'
+import { createBrowserHistory } from 'history'
 
 import {
     FormControl,
@@ -10,8 +14,6 @@ import {
     Grid,
 } from "@material-ui/core";
 
-import usePasswordHooks from '../hooks/usePasswordHooks'
-
 const useStyles = makeStyles((theme) => ({
     root: {
         "& > *": {
@@ -21,34 +23,49 @@ const useStyles = makeStyles((theme) => ({
     },
 }))
 
-function Profile() {
+let history = createBrowserHistory();
+
+function Profile(props) {
     const classes = useStyles();
     const [isButtonDisabled, setIsButtonDisabled] = useState(true)
-
-    const [
-        password,
-        setPassword,
-        passwordError,
-        errorPasswordMessage,
-        handlePasswordOnBlur
-    ] = usePasswordHooks()
+    
+    const [oldPassword, setOldPassword] = useState("")
+    const [newPassword, setNewPassword] = useState("")
 
     async function handleOnSubmit(e) {
         e.preventDefault()
+        try {
+            let getJwtToken = localStorage.getItem("jwtToken")
+            let decodedJwtToken = jwtDecode(getJwtToken)
+            console.log(decodedJwtToken)
+            let success = await axios.put(
+                "http://localhost:3001/users/update-password",
+                {
+                    userName: decodedJwtToken.username,
+                    oldPassword: oldPassword,
+                    newPassword: newPassword,
+                },
+                {
+                    headers: {authorization: `Bearer ${getJwtToken}`}
+                }
+            )
+            props.history.push('/login')
+        } catch (error) {
+            toast.error(error.response.data, {
+                position: "top-right",
+                autoClose: 5000,
+                hideProgressBar: false,
+                closeOnClick: true,
+                pauseOnHover: true,
+                draggable: true,
+                progress: undefined,
+            })
+        }
     }
 
-    let lengthChecker = password.length > 0
-
-    let errChecker = passwordError
-
     useEffect(() => {
-        if (lengthChecker && !errChecker) {
-            setIsButtonDisabled(false)
-        } else {
-            setIsButtonDisabled(true)
-        }
-
-    }, [password])
+        setIsButtonDisabled(false)
+    }, [])
 
     return (
         <Grid
@@ -61,18 +78,16 @@ function Profile() {
         >
             <Grid item xs={12}>
                 <form className={classes.root} autoComplete="on" onSubmit={handleOnSubmit}>
-                    <FormControl error={passwordError}>
+                    <FormControl>
                         <InputLabel htmlFor="component-password">Password</InputLabel>
                         <Input
                             type="password"
-                            id="component-password"
-                            name="password"
-                            value={password}
-                            onChange={(e) => setPassword(e)}
-                        // onBlur={() => handlePasswordOnBlur()}
+                            id="component-oldPassword"
+                            name="oldPassword"
+                            value={oldPassword}
+                            onChange={(e) => setOldPassword(e.target.value)}
                         />
                         <FormHelperText id="component-error-text">
-                            {passwordError && errorPasswordMessage}
                         </FormHelperText>
                     </FormControl>
                     <br />
@@ -82,6 +97,8 @@ function Profile() {
                             type="password"
                             id="component-newPassword"
                             name="newPassword"
+                            value={newPassword}
+                            onChange={(e) => setNewPassword(e.target.value)}
                         />
                     </FormControl>
                     <br />
@@ -95,7 +112,6 @@ function Profile() {
             </Grid>
         </Grid>
     )
-
 }
 
 export default Profile
